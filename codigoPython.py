aff987e5fc9c96a63e1db47a6570a9a225a516cf
CarlosEdu <cepc@cesar.school>
2022-11-14 19:56:25 -0300

    Código atualizado do sistema
    (ainda falta implementar a manipulação de arquivos)

--- codigoPython.py
+++ codigoPython.py
@@ -1,45 +1,96 @@
+
+#Simulação de telas do protótipo por meio do terminal (esboço de código do protótipo de média fidelidade)
+#Do lado esquerdo das opções da tela inicial, terá um pegueno widget ocupando 1/4 da tela inicial, mostrando ao hóspede os 
+#principais avisos e eventos do hotel.
+
 import os
 
-def menu():
+#Antes de exibir o menu, serão exibidas telas de apresentação e boas vindas, com um mascote no centro e balões de fala 
+#no lado superior esquerdo do mesmo. Melhor vizulaização no protótipo do figma.
+def tutorial():
     os.system('cls')
+    print('Para prosseguir para a próxima tela, precione ENTER para continuar\n\n')
+    input('Olá, boa Tarde!\nSeja bem vindo(a) NOME, ao Serviço de Quarto virtual do Saint Patrick Grand Hotel! (...)')
+    os.system('cls')
+
+    input('Me chamo Fernando e serei seu assistente virtual\ndurante sua estadia no nosso hotel! (...)')
+    os.system('cls')
+
+    input('Este é seu canal de comunicação direto com os nossos funcionários do Hotel (...)')
+    os.system('cls')
+
+    input('Esse é o lugar onde você tem a possibilidade de solicitar\ntrocas de enxováis, conversar diretamente\n'
+        'com um funcionário e utilizar nosso controle universal por exemplo. (...)')
+    os.system('cls') 
+
+    print('Esse é o lugar onde você tem a possibilidade de solicitar\ntrocas de enxováis, conversar diretamente',
+        'com um funcionário e utilizar nosso controle universal por exemplo.')
+    input('\nAqui também tem indicações de restaurantes parceiros do nosso hotel personalizados para você!')
+    menu()
+
+def menu(): #tela inicial
+    os.system('cls')
+    print('Menu principal')
+    #O "widget do tempo" estará no seu modo minimizado no canto superior direito da tela inicial
+    #que ao ser clicado, se expande e sobrepõe de maneira transparente a tela inicial (menu)
     cmd = int(input("""
-    1 - Comedorias e Restaurantes parceiros
-    2 - Troca de Enxovais (Lençol, Toalhas, Travesseiros, Tapetes...)
-    3 - Controle dos Dispositivos do Quarto (Tv, Ar-Condicionado, Luz, Cortina)
-    4 - Informações Turisticas (Locais Próximos, Praias, Parques...)
-    5 - Informações do Hotel (Eventos, Horários das Refeições)
-    6 - Serviços adicionais (Parceiros do Hotel: Serviços de Massagem, Passeios de Bug, Fotográfos...)
-
-Digite o número que deseja: """))
-
-    if cmd == 1:
-        tela1()
+    0 - Abrir Widget do Tempo 
+    1 - Serviços de Quarto
+    2 - Controle
+    3 - Especialmente pra voçê
+    4 - Conheça o Hotel
+    5 - Ajuda
+    
+    Digite o número que deseja: """))
+    if cmd == 0:
+        widget_tempo()
+    elif cmd == 1:
+        servicos()
     elif cmd == 2:
-        tela2()
+        controle()
     elif cmd == 3:
-        tela3()
+        turismo()
     elif cmd == 4:
-        tela4()
+        info_hotel()
     elif cmd == 5:
-        tela5()
-    elif cmd == 6:
-        tela6()
+        assistencia()
     else:
         os.system('cls')
         print("Você digitou um comando inválido, digite novamente uma das opções abaixo:")
         menu()
 
-def tela1():
-    os.system('cls')
-    print('Comedorias e Restaurantes parceiros:\n\n')
-    print('1. Filé do Zezé \n2. Najuany Bistrô\n3. Basílico Ristorante\n4. Bodega do Sertão\n5. Del Mare')
-    input('\n\nPressione ENTER para voltar ao Menu')
-    menu()
+def servicos(): #Serviços de Quarto
+    #No canto inferior da tela, terá o mascote e um balão de fala escrito: 
+    #"Caso não encontre o que deseja, você pode conversar com um de nossos funcionários!"
 
-def tela2():#troca de enxovais
+    os.system('cls')
+    print('Serviços de Quarto:\n')
+    print('1.Solicitar um funcionário      2.Limpeza de Quarto')
+    print('3.Troca de Enxoval              4.Chamar Assistência Técnica')
+    print('5.Pedir Reposição de Frigobar   6.Converse com um funcionário')
+
+    cmd = int(input('\n\nInira a opção que deseja ou pressione ENTER para voltar ao Menu: '))
+
+    if cmd == 1:
+        input('Um funcionário já está a caminho!')
+    elif cmd == 2:
+        input('A sua solicitação da limpeza do quarto já foi encaminhada')
+    elif cmd == 3:
+        enxoval()
+    elif cmd == 4:
+        input('A assistência já está a caminho do seu quarto!')
+    elif cmd ==5:
+        input('Sua solicitação de repor o frigobar já foi encaminhada')
+    elif cmd ==6:
+        input('Encaminhando você para o chat com um de nossos funcionários...')
+        #Abrir uma nova tela com um chat com algum funcionário da recepção.
+    else:
+        menu()
+
+def enxoval():#troca de enxovais
     while True:
         os.system('cls')
-        print('você abriu a tela 2')
+        print('Troca de Enxoval')
         cmd = int(input("""
         1 - Troca de Travesseiros
         2 - Solicitar troca de toalhas
@@ -49,24 +100,29 @@ def tela2():#troca de enxovais
 
         Digite o número que deseja: """))
         
+        #Qualquer pedido do enxoval será enviado diretamente ao sistema da recepção, já identificando o quarto
         if cmd == 1:#troca de travesseiros
             os.system('cls')
-            print('Seus travesseiros serão trocados!!')
+            qntd = int(input('Quantos travesseiros você deseja? '))
+            print(f'Estamos encaminhando {qntd} travesseiros para o seu quarto!')
             input('\nPressione ENTER para voltar à Troca de enxovais')
 
         elif cmd == 2:#troca de toalhas
             os.system('cls')
-            print('Suas toalhas serão trocadas!!')
+            qntd = int(input('Quantas toalhas você deseja? '))
+            print(f'Estamos encaminhando {qntd} toalhas para o seu quarto!')
             input('\nPressione ENTER para voltar à Troca de enxovais')
 
         elif cmd == 3:#troca de roupas de cama
             os.system('cls')
-            print('Suas roupas de cama serão trocadas!!')
+            qntd = int(input('Quantas roupas de cama você deseja? '))
+            print(f'Estamos encaminhando {qntd} roupas de cama para o seu quarto!')
             input('\nPressione ENTER para voltar à Troca de enxovais')
 
-        elif cmd == 4:
+        elif cmd == 4:#troca de tapetes
             os.system('cls')
-            print('Seus tapetes serão trocados!!')
+            qntd = int(input('Quantos tapetes você deseja? '))
+            print(f'Estamos encaminhando {qntd} tapetes para o seu quarto!')
             input('\nPressione ENTER para voltar à Troca de enxovais')
 
         elif cmd == 5:
@@ -75,8 +131,35 @@ def tela2():#troca de enxovais
         else:
             os.system('cls')
             print('Seu comando digitado é inválido')
-            tela2()
+            enxoval()
 
+
+def controle():#Controle do Quarto
+    os.system('cls')
+    print('Controle do Quarto\n')
+    cmd = int(input("""
+    1 - Televisão
+    2 - Persianas
+    3 - Luzes
+    4 - Ar Condicionado
+    5 - Voltar ao menu
+
+    Digite o número que deseja: """))
+
+    if cmd == 1:
+        televisao()
+    elif cmd == 2:
+        persianas()
+    elif cmd == 3:
+        luzes()
+    elif cmd == 4:
+        temperatura()
+    elif cmd == 5:
+        menu()
+    else:
+        os.system('cls')
+        input("Você digitou um comando invalido, digite novamente uma das opções abaixo:")
+        controle()
 
 def televisao():
     os.system('cls')
@@ -94,7 +177,7 @@ def televisao():
     if cmd == 1:
         os.system('cls')
         print('Os seguintes canais estão disponíveis:')
-        print('1. Disney Channel \n2. HBO \n3. Albion Online')
+        print('1.Disney Channel \n2.HBO \n3.Discovery Channel \n4.Globo')
         input('Pressione ENTER para voltar ao menu Televisão')
         televisao()
     elif cmd == 2:
@@ -120,13 +203,12 @@ def televisao():
         input('\nPressione ENTER para voltar ao menu Televisão')
         televisao()
     elif cmd == 6:
-        tela3()
+        controle()
     else:
         os.system('cls')
         print("Você digitou um comando invalido")
         input('Pressione ENTER para voltar ao menu Televisão')
         televisao()
-
 
 def persianas():
     os.system('cls')
@@ -147,13 +229,12 @@ def persianas():
         input('\nPressione ENTER para voltar ao menu Persianas')
         persianas()
     elif cmd == 3:
-        tela3()
+        controle()
     else:
         os.system('cls')
         print("Você digitou um comando invalido")
         input('Pressione ENTER para voltar ao menu Persianas')
         persianas()
-
 
 def luzes():
     os.system('cls')
@@ -187,7 +268,7 @@ def luzes():
         input('\nPressione ENTER para voltar ao menu luzes')
         luzes()
     elif cmd == 5:
-        tela3()
+        controle()
     else:
         os.system('cls')
         print("Você digitou um comando invalido")
@@ -214,95 +295,74 @@ def temperatura():
         temperatura()
     elif cmd == 3:
         os.system('cls')
-        tela3()
+        controle()
     else:
         os.system('cls')
         print("Você digitou um comando invalido")
         input('Pressione ENTER para voltar ao menu temperatura')
         temperatura()
 
-
-def tela3():#Controle do Quarto
+def turismo(): #Especialmente pra você (Informações turísticas)
     os.system('cls')
-    cmd = int(input("""
-    1 - Televisão
-    2 - Persianas
-    3 - Luzes
-    4 - Temperatura
-    5 - Voltar ao menu
+    print('Especialmente Pra Você\n')
+    print('1.Locais Próximos\n2.Conheça Nossos Parceiros\n3.Atividades Turísticas pra Você e Sua Família')
+    #Cada tela das opções a cima terá informações claras e objetivas sobre a avaliação, localização e horário de
+    # funcionamento. Além de preços dos pacotes de passeios turísticos.     
 
-    Digite o número que deseja: """))
-
-
-    if cmd == 1:
-        televisao()
-    elif cmd == 2:
-        persianas()
-    elif cmd == 3:
-        luzes()
-    elif cmd == 4:
-        temperatura()
-    elif cmd == 5:
-        menu()
-    else:
-        os.system('cls')
-        print("Você digitou um comando invalido, digite novamente uma das opções abaixo:")
-        menu()
-
-
-
-def tela4():
-    os.system('cls')
-    print('Pontos Turísticos:\n\n')
-    print('1. Museu da História Natural \n2. Centro Cultural de Pajucara\n3. Praia do Francês\n4. Ponta Negra\n5. Praia do Gunga\n6. Museu do Theo Brandão')
     input('\n\nPressione ENTER para voltar ao Menu')
     menu()
 
-def tela5():
+def info_hotel(): #Conheça o Hotel (info e avisos sobre o hotel)
     os.system('cls')
-    print('Horários do hotel:')
-    print('Entrada e saída permitidos a qualquer horário do dia\n Funcionamos 24/7\n\n')
-    print('Horários das refeições:\n\n')
-    print('1. Café da manhã: 7:00 - 10:30 \n2. Almoço: 11:00 - 13:30 \n3. Lanche da tarde: 15:00 - 16:30 \n4. Janta: 19:30 - 22:00 ')
+    print('Conheça o Hotel\n\n')
+    print('Avisos Relevantes:\n\n1.Aviso1\n2.Aviso2') #exibir avisos relevantes sobre o hotel, opção personalizável.
+    print('\nHorários do hotel:')
+    print('\n-Entrada e saída permitidos a qualquer horário do dia\n')
+    print('Horários das refeições:')
+    print('\n1. Café da manhã: 7:00 - 10:30 \n2. Almoço: 11:00 - 13:30 \n3. Lanche da tarde: 15:00 - 16:30 \n4. Janta: 19:30 - 22:00 ')
     input('\n\nPressione ENTER para voltar ao Menu')
     menu()
 
-
-def solicitarFuncionario():
+def assistencia():
     os.system('cls')
+    print('Ajuda')
     cmd = int(input("""
-        1 - Repor Frigobar
-        2 - Recolher Lixo
-        3 - Limpeza
+        1 - Chat com Funcionário
+        2 - Emergência Médica
+        3 - Como Funciona o Nosso Sistema
         4 - Voltar ao menu
 
         Digite o número que deseja: """))
 
     if cmd == 1:
         os.system('cls')
-        print('Em breve seu frigobar seráreposto. Funcionários à caminho!!')
-        input('\n\nPressione ENTER para voltar aos serviços adicionais')
-        menu()
+        print('Encaminhando você para o chat com um de nossos funcionários...')
+        input('\n\nPressione ENTER para voltar ao menu de assistência')
+        assistencia()
     elif cmd == 2:
         os.system('cls')
-        print('O lixo será recolhido em breve. Aguarde um momento!!')
-        input('\n\nPressione ENTER para voltar aos serviços adicionais')
-        menu()
+        print('Seu chamado emergêncial foi enviado para a recepção, não se preocupe \nprofissionais já estão a caminho')
+        input('\n\nPressione ENTER para voltar ao menu de assistência')
+        assistencia()
     elif cmd == 3:
         os.system('cls')
-        print('O serviço de limpeza está se dirigindo ao seu quarto!!')
-        input('\n\nPressione ENTER para voltar aos serviços adicionais')
-        menu()
+        input('\n\nPressione ENTER para rever o tutorial inicial')
+        tutorial()
     elif cmd == 4:
         menu()
     else:
         os.system('cls')
-        print("Você digitou um comando invalido, digite novamente uma das opções abaixo:")
-        menu()
+        input("Você digitou um comando invalido, precione ENTER e digite novamente uma das opções válidas:")
+        assistencia()
 
+def widget_tempo():
+    os.system('cls')
+    print('Widget do Tempo\n')
+    print('Precione ENTER quando quiser voltar ao menu\n\n')
+    input('Maceió, Alagoas   | 18ºGraus'
+        '\n08 de Novembro    | 18:40'
+        '\nHoje irá chover!  | Previsão Semanal') #Ao lado mostrar tabelinha dos dias da semana e suas previsões temporais
+    menu()
 
-
-def tela6():
-    solicitarFuncionario()
-
+tutorial()
 menu()
